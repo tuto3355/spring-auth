5dab748f99855b1c0d280810f7f06310de702860
DESKTOP-HMN2L1J\정강용 <tuto3355@naver.com>
2023-08-29 12:51:26 +0900

    spring security jwt 로그인

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -37,14 +37,4 @@ public class UserController {
         return "redirect:/api/user/login-page";
     }
 
-    @PostMapping("/user/login")
-    public String longin(LoginRequestDto requestDto, HttpServletResponse res) {
-        try {
-            userService.login(requestDto, res);
-        } catch (Exception e) {
-            return "redirect:/api/user/login-page?error";
-        }
-
-        return "redirect:/";
-    }
 }
